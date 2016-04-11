8f86ef34e885c3d059634e5515f42eb17d579a1a
Christoph Fischer <chris@toph.de>
2016-04-11 20:04:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Prefix replacement
+Yourls plugin to expand urls prefixed with a certain keyword
+
+## Examples
+- `http://short.url/prefix-foo-bar` could be turned into `http://my.long.url/whatever/foo-bar`
+- `http://short.url/date-161231` could be turned into `http://my.long.url/events/2016-12-31`
+- `http://short.url/just-a-title` could be turned into `http://my.long.url/just/?next=a&word=title`
+
+## Configuration
+All configuration takes place in the plugin.yaml file.
+
+## Processing
+Processing will take part in four stages:
+1. The url keyword will be passed through a number of definable text replacements. These can be found in the `replacements` section in plugin.yaml. Each element in the list there takes two configurations: `find` and `replace`.
+2. The correct configuration will be determined by matching the start of the url keyword to one of the `prefix` definitions under `prefixes` in plugin.yaml. The prefix will then be cut off from the keyword.
+3. The rest of the keyword will be processed according to the further configuration for this prefix:
+  1. If the prefix has a `split` configuration set, the delimiter defined there will be used to split the keywords into parts.
+  2. If the prefix has a `subparts` section, all elements listed there will be used as `start: length` parameters for PHP's `substr()` function and a list of parts will be created accordingly.
+  3. If nothing like the above is defined, the keyword will remain one single part.
+4. A long url will be constructed by inserting the above keyword or its parts into the prefix's `url` through PHP's `sprintf()` function.
+
+Have a look at the enclosed plugin.yaml for some examples.
+
+## Author
+Christoph Fischer (chris@toph.de) for Volksmission Freudenstadt (http://www.volksmission-freudenstadt.de)
---
+++ plugin.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+  Plugin Name: Prefix replacement
+  Plugin URI: http://open.vmfds.de/
+  Description: Replace and expand prefixed urls
+  Version: 0.1
+  Author: Christoph Fischer <christoph.fischer@volksmission.de>
+  Author URI: http://www.chris.toph.de/
+ */
+
+// No direct call
+if (!defined('YOURLS_ABSPATH'))
+    die();
+error_reporting(E_ALL);
+
+yourls_add_filter('shunt_get_keyword_info', 'vmfds_prefix_get_keyword_info');
+
+/**
+ * Check if this is a short url for a series
+ * @param string $existingValue Value from previous filters
+ * @param string $keyword Url Keyword
+ * @param string $field Keyword field
+ * @param string $notfound Filter
+ * @return string Url or previous value
+ */
+function vmfds_prefix_get_keyword_info($existingValue, $keyword, $field, $notfound)
+{
+    // Important! Safeguard existing value
+    $result = $existingValue;
+
+    $configurationFile = YOURLS_ABSPATH.'/user/plugins/vmfds-prefix/plugin.yaml';
+    if (file_exists($configurationFile)) 
+        $configuration = yaml_parse_file($configurationFile);
+    
+    // we only treat url fields
+    if ($field == 'url') {
+        
+        // Step 1: Replacements
+        if ((isset($configuration['replacements'])) && (is_array($configuration['replacements']))) {
+            foreach ($configuration['replacements'] as $replacement) {
+                if ((isset($replacement['find'])) && (isset($replacement['replace'])))
+                    $keyword = str_replace($replacement['find'], $replacement['replace'], $keyword);
+            }
+        }
+        
+        // Step 2: Try prefixed keywords
+        foreach ($configuration['prefixes'] as $p) {
+            $prefix = $p['prefix'];
+        if (substr($keyword, 0, strlen($prefix))==$prefix) {
+                $keyword = substr($keyword, strlen($prefix));
+                if (is_array($p['subparts'])) {
+                    $subParts = [];
+                    foreach ($p['subparts'] as $b => $c) {
+                        $subParts[] = substr($keyword, $b, $c);
+                    }
+                    $result = vsprintf($p['url'], $subParts);
+                } elseif (isset($p['split'])) {
+                    $result = vsprintf($p['url'], explode($p['split'], $keyword));
+                } else {
+                    $result = sprintf($p['url'], $keyword);
+                }
+                continue;
+            }
+        }
+    }
+    return $result;
+}
